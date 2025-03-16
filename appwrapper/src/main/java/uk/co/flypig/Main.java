b158906e1a5c88f1318f2c8928d19d92ab1bba04
David Llewellyn-Jones <david@flypig.co.uk>
2025-03-16 19:03:52 +0000

    Add hashmap invocation
    Adds support for invoking methods defined in a HashMap with different
    input and output classes. The invoke() function automatically
    deserialises and serialises the input and output from and to JSON for
    crossing the Java-native boundary.
    
    Includes a couple of example functions helloWorld() and worldHello().

--- appwrapper/src/main/java/uk/co/flypig/Main.java
+++ appwrapper/src/main/java/uk/co/flypig/Main.java
@@ -24,10 +24,38 @@ import org.schabi.newpipe.extractor.StreamingService;
 import org.schabi.newpipe.extractor.ServiceList;
 import org.schabi.newpipe.extractor.stream.StreamExtractor;
 import org.schabi.newpipe.extractor.stream.VideoStream;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import okhttp3.OkHttpClient;
 import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.function.Function;
+import com.fasterxml.jackson.core.type.TypeReference;
+import org.graalvm.nativeimage.IsolateThread;
+import org.graalvm.nativeimage.UnmanagedMemory;
+import org.graalvm.nativeimage.c.function.CEntryPoint;
+import org.graalvm.nativeimage.c.type.CTypeConversion;
+import org.graalvm.nativeimage.c.type.CIntPointer;
+import org.graalvm.nativeimage.c.type.CCharPointer;
+import org.graalvm.nativeimage.c.type.CCharPointerPointer;
+
+final class MethodInfo<In, Out> {
+    public MethodInfo(Function<In, Out> method, Class<In> className) {
+        this.method = method;
+        this.className = className;
+    }
+
+    Function<In, Out> method;
+    Class<In> className;
+}
 
 final class Main {
+    static ObjectMapper jsonMapper = new JsonMapper();
+    static Map<String, MethodInfo> methodInfo;
+    static CCharPointer emptyString;
+
     private Main() {
         throw new UnsupportedOperationException();
     }
@@ -70,6 +98,56 @@ final class Main {
         }
 
         System.out.println("Completed");
+    }
+
+    private static <In, Out> CCharPointer call(Function<In, Out> method, CCharPointer in, Class<In> className) {
+        String output = new String();
+        try {
+            final String parameters = CTypeConversion.toJavaString(in);
+            final In input = Main.jsonMapper.readValue(parameters, className);
+            final Out result = method.apply(input);
+            output = Main.jsonMapper.writeValueAsString(result);
+        } catch (final JsonProcessingException e) {
+            System.out.println("Exception: " + e);
+        }
+
+        final CTypeConversion.CCharPointerHolder holder = CTypeConversion.toCString(output);
+        return holder.get();
+    }
+
+    private static HelloWorldOut helloWorld(HelloWorldIn input) {
+        final String hello = "Hello " + input.name;
+        final HelloWorldOut result = new HelloWorldOut();
+        result.result = hello;
+        return result;
+    }
+
+    private static HelloWorldIn worldHello(HelloWorldOut input) {
+        final String hello = input.result + " Hello";
+        HelloWorldIn result = new HelloWorldIn();
+        result.name = hello;
+        return result;
+    }
+
+    @CEntryPoint(name = "init")
+    static void init(IsolateThread thread) {
+        emptyString = CTypeConversion.toCString("").get();
+        methodInfo = new HashMap<>();
+        methodInfo.put("helloWorld", new MethodInfo<HelloWorldIn, HelloWorldOut>(Main::helloWorld, HelloWorldIn.class));
+        methodInfo.put("worldHello", new MethodInfo<HelloWorldOut, HelloWorldIn>(Main::worldHello, HelloWorldOut.class));
+    }
+
+    @CEntryPoint(name = "invoke")
+    public static CCharPointer invoke(IsolateThread thread, CCharPointer methodNamePtr, CCharPointer in) {
+        final String methodName = CTypeConversion.toJavaString(methodNamePtr);
+        CCharPointer result = emptyString;
+        if (methodInfo.containsKey(methodName)) {
+            final MethodInfo info = methodInfo.get(methodName);
+            result = call(info.method, in, info.className);
+        } else {
+            System.out.println("Invocation failed, unknown method: " + methodName);
+        }
+        return result;
     }
 }
 
